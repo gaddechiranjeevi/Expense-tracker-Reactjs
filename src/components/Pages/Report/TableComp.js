0fb9dd8ab344955aa89dee3e2cbc7acb083be6a3
gaddechiranjeevi <gaddechiru@gmail.com>
2022-11-28 20:08:17 +0530

    Premium features

---
+++ src/components/Pages/Report/TableComp.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TableBodyComp from "./TableBody";
+
+const TableComp = (props) => {
+  const { TableValues } = props;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>TableValues.one</TableCell>
+            <TableCell align="right">TableValues.two</TableCell>
+            <TableCell align="right">TableValues.three</TableCell>
+            <TableCell align="right">TableValues.four</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {props.expenseList.map((item) => {
+            return <TableBodyComp item={item}/>;
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TableComp;
--- src/components/Pages/Report/index.js
+++ src/components/Pages/Report/index.js
@@ -1,11 +1,70 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import Card from "../../../UI/Card";
+import "./Report.css";
+import TableComp from "./TableComp";
+import moment from "moment";
+import axios from "axios";
 
-const Report = () => {
-    return( 
-        <div>
+const Report = () => { 
+    const [expenseList, setExpenseList] = useState([]);
+    const date = moment().format('DD-MM-YYYY')
+    const token = localStorage.getItem("JWTTOKEN");
 
+    const expenseTableValues = {
+      one: "Date",
+      two: "Description",
+      three: "Category",
+      four: "Expense"
+    }
+    const yealyTableValues = {
+      one: "Month",
+      two: "Income",
+      three: "Expenses",
+      four: "Savings"
+    }
+
+    const fetchReport = async() => {
+      try{
+        const response = await axios.post("http://localhost:3000/auth/api/report/expense",'',  { headers: { Authorization: token } });
+        console.log(response.data.response)
+        setExpenseList(response.data.response)
+      } catch (err){
+        console.log(err)
+      }
+    }
+useEffect(()=>{
+  fetchReport();
+},[])
+  return (
+    <div className="image-container">
+      <div className="heading-container">
+        <h1>Day to Day Expenses</h1>
+      </div>
+      <div className="dateinfo">
+      <label>{date}</label>
+      <label>{}</label>
+      </div>
+      <Card>
+        <div className="year-container">
+           <h3>2022</h3>
+           <lable>July 2022</lable> 
         </div>
-    )
-}
+        <div className={classes.tablecontainer}>
 
-export default Report
+            <TableComp expenseList={expenseList} TableValues={expenseTableValues} />
+        </div>
+      </Card>
+      <Card>
+      <div className="year-container">
+           <h3>Yearly Report</h3>
+        </div>
+        <div className={classes.tablecontainer}>
+            <TableComp expenseList={expenseList} TableValues={expenseTableValues} />
+        </div>
+    
+      </Card>
+    </div>
+  );
+};
+
+export default Report;
